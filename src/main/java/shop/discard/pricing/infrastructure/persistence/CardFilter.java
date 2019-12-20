e9ccb3771d47900df0c5b89b533f0e48f9f4dfdd
ushakov_ri <ushakov_ri@magnit.ru>
2019-12-20 17:44:38 +0300

    feat: change search by name logic

--- src/main/java/shop/discard/pricing/infrastructure/persistence/CardFilter.java
+++ src/main/java/shop/discard/pricing/infrastructure/persistence/CardFilter.java
@@ -2,27 +2,31 @@ package shop.discard.pricing.infrastructure.persistence;
 
 import shop.discard.pricing.domain.Card;
 
+import java.util.Arrays;
+
 public class CardFilter {
-	private String partOfName;
+	private String name;
 	private String langCode;
 
-	public CardFilter(String partOfName, String langCode) {
-		this.partOfName = partOfName;
+	public CardFilter(String name, String langCode) {
+		this.name = name;
 		this.langCode = langCode;
 	}
 
-	public static CardFilter filterBy(String partOfName, String langCode) {
-		return new CardFilter(partOfName, langCode);
+	public static CardFilter filterBy(String name, String langCode) {
+		return new CardFilter(name, langCode);
 	}
 
 	public boolean filter(Card card) {
-		boolean byName = containsIgnoreCase(card.getName(), partOfName);
+		String[] splitName = card.getName().split(" ");
+		boolean byName = containsIgnoreCase(splitName, name);
 		boolean byLanguage = card.getLanguage().equalsIgnoreCase(langCode);
 		return byName && byLanguage;
 	}
 
-	private boolean containsIgnoreCase(String str, String subString) {
-		return str.toLowerCase().contains(subString.toLowerCase());
+	private boolean containsIgnoreCase(String[] strings, String subString) {
+		return Arrays.stream(strings)
+				.anyMatch(s -> s.toLowerCase().startsWith(subString.toLowerCase()));
 	}
 
 }
